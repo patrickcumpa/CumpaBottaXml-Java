ec054af6696fec5c6ad59b413d2415217e5a3ce9
PatrickCumpa <patrick.cumpa@mpolo.it>
2023-01-10 09:45:24 +0100

    Creazione programma esercizio 32

---
+++ PercorsoDrone/src/percorsodrone/PercorsoDrone.java
@@ -0,0 +1,16 @@
+package percorsodrone;
+
+/**
+ *
+ * @author PatrickCumpa
+ */
+public class PercorsoDrone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
